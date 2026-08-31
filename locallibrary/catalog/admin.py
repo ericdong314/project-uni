583f4dfb7872f31bf99fc38fa47eaeb252e25453
Eric Dong <eric2c@outlook.com>
2026-08-31 10:00:01 +0800

    Finish Part4: Admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,10 +2,33 @@ from django.contrib import admin
 
 from .models import Book, BookInstance, Language, Genre, Author
 
-# Register your models here.
-
-admin.site.register(BookInstance)
 admin.site.register(Language)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(Book)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    # exclude = ['date_of_death']
+    inlines = [BookInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = ((None, {'fields': ('book', 'imprint', 'id')}),
+                 ('Availability', {'fields': ('status', 'due_back')}))
