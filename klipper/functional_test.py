6f54890cc88a6776e2afa8363dd14c0ad076f27c
Eric Dong <eric2c@outlook.com>
2026-09-08 11:30:39 +0800

    Start klipper app, and create basic FT

---
+++ klipper/functional_test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import unittest
+
+
+class TestKlipper(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def test_user_add_website(self):
+        # Eric opens the page 'klipper/' sees the title 'Klipper',
+        self.browser.get('http://localhost:8000/klipper/')
+        self.assertIn('Klipper', self.browser.title)
+
+        # On the page, an input box invites him to enter a URL.
+
+        # Eric enters the link to his favourite website and presses enter.
+
+        # The page updates with the link added to the page as an item in a list.
+        self.fail('To complete')
+
+
+if __name__ == '__main__':
+    unittest.main()
