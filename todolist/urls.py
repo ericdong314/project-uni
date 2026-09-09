fc79cb2a754875e86dc29e9d0728f1d694d2cf92
Eric Dong <eric2c@outlook.com>
2026-09-09 10:29:36 +0800

    using custom view home_page with passing unit tests

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='items/', permanent=True)),
-    path('items/', views.ItemListView.as_view(), name='items'),
-    path('item/create/', views.create_item, name='create_item')
+    path('items/', views.home_page, name='home'),
+    # path('items/', views.ItemListView.as_view(), name='items'),
+    # path('item/create/', views.create_item, name='create_item')
 ]
