5a327f191d075bf54aada455614a0815935a9417
Eric Dong <eric2c@outlook.com>
2026-09-09 09:31:07 +0800

    FT now tests creating to-do items.

--- todolist/functional_tests.py
+++ todolist/functional_tests.py
@@ -1,5 +1,8 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,18 +14,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_create_items(self):
-        # Alison visits the website and notices that it is a to-do list app.
+        # Alison visits the website and notices that the page title and header mention to-do lists.
         self.browser.get('http://localhost:8000/todo/')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She is invited to create a to-do item straight away.
-        # She sees an input box and types in it "Buy a new pen."
-        self.fail('Manual fail')
+        # She is invited to create a to-do item with a text box straight away.
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(input_box.get_attribute('place_holder'), 'Enter a to-do item')
+
+        # She types in it "Buy a new pen."
+        input_box.send_keys('Buy a new pen')
 
         # She then presses enter and the page updates and shows
-        # "1. Buy a new pen." as an item in a to-do list.
+        # "1: Buy a new pen." as an item in a to-do list.
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy a new pen.' for row in rows))
+
+        # These is still an input box inviting he to add another item.
         # She now enters "Buy a notebook." in the input box.
+        self.fail('Finish the test')
 
         # She presses enter and the page updates with both items displayed on the list.
 
