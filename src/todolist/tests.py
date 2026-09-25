dfe1c345c7cb68435f02333b6c23804c6a2d90ef
Eric Dong <eric2c@outlook.com>
2026-09-25 11:07:32 +0800

    Add back test_uses_home_template

--- src/todolist/tests.py
+++ src/todolist/tests.py
@@ -8,6 +8,10 @@ from .views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/todo/')
+        self.assertTemplateUsed(response, 'home.html')
+
     def test_renders_homepage_content(self):
         response = self.client.get('/todo/')
         self.assertContains(response, 'To-Do')
